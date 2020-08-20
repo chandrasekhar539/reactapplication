b3c17a379ae36c4a89ae436a1562fc9a98c46158
chandra sekhar <chanducse11111@gmail.com>
2020-08-20 16:33:09 +0200

    reactapp

---
+++ myapp/src/components/Weather.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+class Weather extends Component {
+  state = {
+    loading: true,
+    person: null,
+  };
+
+  async componentDidMount() {
+    const url = "https://api.randomuser.me/";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ loading: false, person: data.results[0] });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.loading ? (
+          <div>loading...</div>
+        ) : (
+          <div>
+            <div className="title col-sm-12">
+              {this.state.person.name.title}
+            </div>
+            <div className="first col-sm-12">
+              {this.state.person.name.first}
+            </div>
+            <div className="last col-sm-12">{this.state.person.name.last}</div>
+            <img src={this.state.person.picture.large} className="image" />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Weather;
